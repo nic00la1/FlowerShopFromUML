f0b858434ca706664f3f3c746154f980326280be
Nicola Kaleta <nicola.kaleta2@teb.edu.pl>
2025-01-03 09:42:33 +0100

    shop class

--- Shop.cs
+++ Shop.cs
@@ -13,5 +13,21 @@ namespace FlowerShopFromUML
         public List<Flower> Flowers { get; set; }
         public List<Bouquet> Bouquets { get; set; }
         public List<Order> Orders { get; set; }
+
+        public Shop() 
+        {
+            Flowers = new List<Flower>();
+            Bouquets = new List<Bouquet>();
+            Orders = new List<Order>();
+        }
+
+        public Shop(string name, string adress, List<Flower> flowers, List<Bouquet> bouquets, List<Order> orders)
+        {
+            Name = name;
+            Adress = adress;
+            Flowers = flowers ?? new List<Flower>(); // Jesli nie ma kwiatkow to utworz nowa liste kwiatow
+            Bouquets = bouquets ?? new List<Bouquet>(); // -,,- 
+            Orders = orders ?? new List<Order>(); // -,,-
+        }
     }
 }
